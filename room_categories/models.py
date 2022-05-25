b72834859e60346dea4cc005f221f2882dfc8cb9
Victor Scherer <victor_scherer@hotmail.com>
2022-05-25 16:03:13 -0300

    feat: add room_category model

--- room_categories/models.py
+++ room_categories/models.py
@@ -1,3 +1,17 @@
+from uuid import uuid4
+
 from django.db import models
 
-# Create your models here.
+
+class RoomCategory(models.Model):
+    room_category_id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
+    category_name = models.CharField(max_length=255, unique=True, null=False)
+    price = models.FloatField(null=False)
+    max_guest_number = models.IntegerField(null=False)
+
+    reservations = models.ForeignKey(
+        "reservations.Reservation",
+        on_delete=models.CASCADE,
+        related_name="room_category",
+        null=False,
+    )
