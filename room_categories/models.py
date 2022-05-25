66943aa7ff937e0403a610d69e711182261dd3a7
Victor Scherer <victor_scherer@hotmail.com>
2022-05-25 16:18:27 -0300

    fix: field price and category_name

--- room_categories/models.py
+++ room_categories/models.py
@@ -5,8 +5,8 @@ from django.db import models
 
 class RoomCategory(models.Model):
     room_category_id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
-    category_name = models.CharField(max_length=255, unique=True, null=False)
-    price = models.FloatField(null=False)
+    category_name = models.CharField(max_length=150, unique=True, null=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=False)
     max_guest_number = models.IntegerField(null=False)
 
     reservations = models.ForeignKey(
