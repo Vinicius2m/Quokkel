963b38c5dbcea307ac5a8541546022c8ada4c577
Rafael Schug <rafaelschugbc@gmail.com>
2022-05-26 10:13:54 -0300

    fix: charfield attr

--- users/serializers.py
+++ users/serializers.py
@@ -6,11 +6,11 @@ class UserSerializer(serializers.Serializer):
     user_id = serializers.CharField(read_only=True)
     email = serializers.EmailField(required=True)
     password = serializers.CharField(required=True, write_only=True)
-    first_name = serializers.charField(required=True)
-    last_name = serializers.charField(required=True)
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
     age = serializers.IntegerField(required=True)
     cpf = serializers.CharField(required=True, min_length=11, max_length=11)
-    phone = serializers.charField(required=True, min_length=15, max_length=15)
+    phone = serializers.CharField(required=True, min_length=15, max_length=15)
     is_staff = serializers.BooleanField(required=True)
 
 
