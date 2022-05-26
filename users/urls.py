c3d93b0aa106eab60599b47f5b5460768db0277a
Manoela Cunha <manoela_fg@hotmail.com>
2022-05-26 18:37:20 -0300

    feat: users login urls add

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,9 @@
 from rest_framework.urls import path
-from users.views import AdminView
+from users.views import AdminView, GuestsView, UsersView
 
 urlpatterns = [
     path("admin/register/", AdminView.as_view()),
+    path("admin/login/", UsersView.as_view()),
+    path("guests/register/", GuestsView.as_view()),
+    path("guests/login/", UsersView.as_view())
 ]
