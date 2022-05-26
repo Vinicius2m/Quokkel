523f2474b977a631228a141d381f472843209b31
Rafael Schug <rafaelschugbc@gmail.com>
2022-05-26 15:16:18 -0300

    fix: AdminView set_password

--- users/views.py
+++ users/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
-from users.models import User
-
-from users.serializers import UsersSerializer
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from users.models import User
+from users.serializers import UsersSerializer
 
 
 class AdminView(APIView):
@@ -23,6 +23,9 @@ class AdminView(APIView):
             )
 
         user: User = User.objects.create(**serializer.validated_data)
+        user.set_password(serializer.validated_data["password"])
+        user.save()
+
         serializer = UsersSerializer(user)
 
         return Response(serializer.data, status.HTTP_201_CREATED)
