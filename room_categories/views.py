2025ae7f05004a2f26800e0ccac205ef542664f0
Victor Scherer <victor_scherer@hotmail.com>
2022-05-26 14:23:27 -0300

    feat: Room category create add

--- room_categories/views.py
+++ room_categories/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.status import (
+    HTTP_201_CREATED,
+    HTTP_200_OK,
+    HTTP_400_BAD_REQUEST,
+    HTTP_422_UNPROCESSABLE_ENTITY
+)
 
-# Create your views here.
+from .serializers import RoomCategoriesSerializer, UpdateRoomCategoriesSerializer
+from .models import RoomCategory
+
+class RoomCategoriesView(APIView):
+    def post(self, request: Request):
+        serializer = RoomCategoriesSerializer(
+            data=request.data
+        )
+        serializer.is_valid(raise_exception=True)
+
+        founded_room_category = RoomCategory.objects.filter(category_name=request.data['category_name'])
+
+        if founded_room_category:
+            return Response({'error': 'Category name already exists'}, status=HTTP_422_UNPROCESSABLE_ENTITY)
+
+        room_category = RoomCategory.objects.create(**serializer.validated_data)
+
+        serializer = RoomCategoriesSerializer(room_category)
+
+        if room_category:
+            return Response(serializer.data, status=HTTP_201_CREATED)
+
+
+        return Response(
+            data=serializer.errors,
+            status=HTTP_400_BAD_REQUEST
+        )
+    
+    
--- users/models.py
+++ users/models.py
@@ -9,12 +9,12 @@ class User(AbstractUser):
     user_id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     email = models.EmailField(unique=True, null=False)
     first_name = models.CharField(unique=False, null=False, max_length=50)
-    last_name = models.charField(unique=False, null=False, max_length=50)
+    last_name = models.CharField(unique=False, null=False, max_length=50)
     age = models.IntegerField(null=False)
-    cpf = models.charField(unique=True, null=False, max_length=11)
+    cpf = models.CharField(unique=True, null=False, max_length=11)
     phone = models.CharField(unique=True, max_length=15)
     is_staff = models.BooleanField(default=False)
-    username = models.charField(unique=False, blank=True, null=True, max_length=50)
+    username = models.CharField(unique=False, blank=True, null=True, max_length=50)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
