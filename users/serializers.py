eafac0ae7b911cc9daa5ff3f7b429d521e91807b
Rafael Schug <rafaelschugbc@gmail.com>
2022-05-26 10:04:06 -0300

    feat: User serializer

---
+++ users/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.Serializer):
+
+    user_id = serializers.CharField(read_only=True)
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
+    first_name = serializers.charField(required=True)
+    last_name = serializers.charField(required=True)
+    age = serializers.IntegerField(required=True)
+    cpf = serializers.CharField(required=True, min_length=11, max_length=11)
+    phone = serializers.charField(required=True, min_length=15, max_length=15)
+    is_staff = serializers.BooleanField(required=True)
