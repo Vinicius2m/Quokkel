ffd0ff3dc8c0ec1a0132e9d346f9e77eba52b8c3
ezms <emanuelzm@protonmail.com>
2022-05-26 10:44:32 -0300

    fix: fixing required fields in room serializers

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
 
-from room_categories.serializers import RoomCategorySerializer
+from room_categories.serializers import RoomCategoriesSerializer
 
 
 class RoomsSerializer(serializers.Serializer):
     room_id = serializers.UUIDField(read_only=True)
-    number = serializers.IntegerField()
+    number = serializers.IntegerField(required=True)
     available = serializers.BooleanField()
 
-    room_category = RoomCategorySerializer(many=False, required=False)
+    room_category = RoomCategoriesSerializer(many=False, required=False)
